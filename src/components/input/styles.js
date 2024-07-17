e54b1b83e1adf41551e04165bb4b205b92b8c6b8
Josiel Jr. <josieljrr.dev@gmail.com>
2024-07-17 17:43:10 -0300

    first commit

---
+++ src/components/input/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    font-family: 'Roboto', sans-serif;
+    font-size: 24px;
+
+    input {
+        width: 100%;
+        height: 75px;
+        padding: 0 10px;
+        border: 0;
+
+        background-color: #AAAAFF;
+        color: #FFF;
+
+        font-family: 'Roboto', sans-serif;
+        font-size: 24px;
+        font-weight: 700;
+        text-align: end;
+    }
+`
---
+++ src/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #CACACA;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Content = styled.div`
+    width: 70%;
+    background-color: #FFFF;
+`
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`
